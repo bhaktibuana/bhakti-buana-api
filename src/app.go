1338581a69c626cb3f3e3fc215d3f0e80b62a346
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-05-05 14:07:11 +0700

    shared: Global - update cors

--- src/app.go
+++ src/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"bhakti-buana-api/src/configs"
 	"bhakti-buana-api/src/database"
 	"bhakti-buana-api/src/routers"
 	"fmt"
@@ -34,10 +35,11 @@ func Middlewares(app *gin.Engine) {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
+		// AllowAllOrigins:  true,
 		AllowCredentials: true,
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowOrigins:     []string{configs.ClientConfig().CLIENT_URL},
 	}))
 }
 
